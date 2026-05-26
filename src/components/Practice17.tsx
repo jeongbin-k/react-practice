5c285850a67b60b8aeb737b741653cefd53642d0
Jeongbin Kang <kangbin8109@gmail.com>
2026-05-26 20:45:40 +0900

    API 연동 + 로딩/에러 처리

--- src/components/Practice17.tsx
+++ src/components/Practice17.tsx
@@ -4,6 +4,19 @@ function Practice17() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/9999")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
   return <div></div>;
 }
 
